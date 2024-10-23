8e84889e3407bbbbb8c4e4d1820c7c8e292d8e95
Dmehta06 <dipak.mehta@ufl.edu>
2024-10-23 16:28:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+#Dipak Mehta 
 
+def encode(password):
+    encoded_password = ""
+    for x in password:
+        for _ in range(3):
+            if x == "9":
+                x = "0"
+            else:
+                x = str(int(x) + 1)
+        encoded_password = encoded_password + x
+    return encoded_password
+
+
+def decode(password):
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        print("Encode")
+        print ("Decode")
+        selection = int(input("Menu Selection: "))
+
+        if selection == 1:
+            print(f"\nEncoded Password: {encode(input("Password: "))}")
+        elif selection == 2:
+            print(f"\nDecoded Password: {decode(input("Password: "))}")
+        else:
+            print("Invalid Selection")
